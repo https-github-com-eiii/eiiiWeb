95a036e8d8ed14d0665e9158118b45131c46b1ce
ghdtldus <ghdtldus03a@gmail.com>
2024-03-18 14:36:17 +0900

    first commit

---
+++ src/main/java/com/example/eiii/eiiii/EiiiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eiii.eiiii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EiiiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EiiiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eiii/eiiii/EiiiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eiii.eiiii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EiiiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
